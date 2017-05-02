43a2f7a68d407d867ab5e655e2d4fabd82f361e0
kocisov <kocis.34@gmail.com>
2017-05-02 11:23:50 +0200

    *test

--- test/test.js
+++ test/test.js
@@ -1,34 +1,82 @@
-const fs = require('fs')
-const { resolve } = require('path')
+const fs = require('fs');
+const { resolve } = require('path');
 
-function dp (pth, done) {
+function dp(pth, done) {
   fs.open(pth, 'r', (err, fd) => {
     if (err) {
-      return done(err)
+      return done(err);
     }
 
-    done()
-  })
+    done();
+  });
+}
+
+function resolveDir(dir) {
+  return resolve(__dirname, dir);
 }
 
 describe('after running wooo', () => {
-  it('should created containers folder', (done) => {
-    dp(resolve(__dirname, '../_test_/containers'), done)
-  })
+  describe('containers', () => {
+    it('should created containers folder', done => {
+      dp(resolveDir('../_test_/containers'), done);
+    });
 
-  it('should created modules folder', (done) => {
-    dp(resolve(__dirname, '../_test_/modules'), done)
-  })
+    describe('App', () => {
+      it('should created containers/App folder', done => {
+        dp(resolveDir('../_test_/containers/App'), done);
+      });
 
-  it('should created static folder', (done) => {
-    dp(resolve(__dirname, '../_test_/static'), done)
-  })
+      it('should created containers/App/index.js file', done => {
+        dp(resolveDir('../_test_/containers/App/index.js'), done);
+      });
+    });
 
-  it('should created store folder', (done) => {
-    dp(resolve(__dirname, '../_test_/store'), done)
-  })
+    describe('Home', () => {
+      it('should created containers/Home folder', done => {
+        dp(resolveDir('../_test_/containers/Home'), done);
+      });
 
-  it('should created index file', (done) => {
-    dp(resolve(__dirname, '../_test_/index.js'), done)
-  })
-})
+      it('should created containers/Home/index.js file', done => {
+        dp(resolveDir('../_test_/containers/Home/index.js'), done);
+      });
+    });
+  });
+
+  describe('modules', () => {
+    it('should created modules folder', done => {
+      dp(resolveDir('../_test_/modules'), done);
+    });
+
+    it('should created modules/index file', done => {
+      dp(resolveDir('../_test_/modules/index.js'), done);
+    });
+
+    it('should created modules/user file', done => {
+      dp(resolveDir('../_test_/modules/user.js'), done);
+    });
+  });
+
+  describe('static', () => {
+    it('should created static folder', done => {
+      dp(resolveDir('../_test_/static'), done);
+    });
+
+    it('should created static/index.css file', done => {
+      dp(resolveDir('../_test_/static/index.css'), done);
+    });
+
+    it('should created static/normalize.css file', done => {
+      dp(resolveDir('../_test_/static/normalize.css'), done);
+    });
+  });
+
+  describe('store', () => {
+    it('should created store folder', done => {
+      dp(resolveDir('../_test_/store'), done);
+    });
+
+    it('should created store/index file', done => {
+      dp(resolveDir('../_test_/index.js'), done);
+    });
+  });
+});
